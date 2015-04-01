5ada9f8eb38cd58e678611dc6713acff8c0d9b57
kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2015-04-01 14:51:31 +0200

    Update dependencies

--- rosinstall/install_scripts/install_package_dependencies.sh
+++ rosinstall/install_scripts/install_package_dependencies.sh
@@ -12,7 +12,20 @@ ros-$ROS_DISTRO-desktop \
 ros-$ROS_DISTRO-serial \
 ros-$ROS_DISTRO-hector-slam \
 ros-$ROS_DISTRO-hector-localization \
-ros-$ROS_DISTRO-server"
+ros-$ROS_DISTRO-server
+ros-$ROS_DISTRO-fcl \
+ros-$ROS_DISTRO-image-pipeline \
+ros-$ROS_DISTRO-octomap-msgs \
+ros-$ROS_DISTRO-octomap-ros \
+ros-$ROS_DISTRO-octomap-rviz-plugins \
+ros-$ROS_DISTRO-ompl \
+ros-$ROS_DISTRO-moveit-ros \
+ros-$ROS_DISTRO-moveit-resources \
+ros-$ROS_DISTRO-moveit-planners-ompl \
+ros-$ROS_DISTRO-moveit-simple-controller-manager \
+ros-$ROS_DISTRO-ros-control \
+ros-$ROS_DISTRO-ros-controllers \
+ros-$ROS_DISTRO-urdfdom-py"
 
 sudo apt-get -y install $PACKAGES_TO_INSTALL
 
