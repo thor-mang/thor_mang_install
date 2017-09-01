ce02e2721640d769d870c32e165bf458d2d50007
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2017-09-01 17:55:12 +0200

    update moveit install

--- rosinstall/install_scripts/install_package_dependencies.sh
+++ rosinstall/install_scripts/install_package_dependencies.sh
@@ -20,14 +20,11 @@ ros-$ROS_DISTRO-octomap-msgs \
 ros-$ROS_DISTRO-octomap-ros \
 ros-$ROS_DISTRO-octomap-rviz-plugins \
 ros-$ROS_DISTRO-ompl \
-ros-$ROS_DISTRO-moveit-ros \
-ros-$ROS_DISTRO-moveit-resources \
-ros-$ROS_DISTRO-moveit-planners-ompl \
-ros-$ROS_DISTRO-moveit-simple-controller-manager \
+ros-$ROS_DISTRO-moveit \
+ros-$ROS_DISTRO-moveit-kinematics \
 ros-$ROS_DISTRO-ros-control \
 ros-$ROS_DISTRO-ros-controllers \
 ros-$ROS_DISTRO-camera-info-manager \
-ros-$ROS_DISTRO-moveit-commander \
 ros-$ROS_DISTRO-urdfdom-py \
 ros-$ROS_DISTRO-urg-node \
 ros-$ROS_DISTRO-joy \
