f6e1590da2d6e294265570fb6727381aca5e9cec
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2017-09-20 20:02:15 +0200

    cleanups

--- rosinstall/install_scripts/install_mpc_system_script.sh
+++ rosinstall/install_scripts/install_mpc_system_script.sh
@@ -3,23 +3,22 @@
 echo "WARNING: You are going to overwrite system settings! This operation cannot be undone!"
 echo -n "ARE YOU SURE TO PROCEED? [y/N]"
 read -N 1 REPLY
-echo
-if [[ ! ("$REPLY" = "y" || "$REPLY" = "Y") ]]; then
 
+if [[ "$REPLY" = "y" || "$REPLY" = "Y" ]]; then
+  cd /
+
+  # remove default config files
+  sudo rm etc/network/interfaces
+  sudo rm etc/rc.local
+  sudo rm etc/screenrc
+
+  sudo git init
+  sudo git remote add origin https://github.com/thor-mang/system_mpc_ubuntu.git
+  sudo git fetch
+  sudo git checkout -t origin/master
+
+  echo "Installation completed! Please reboot system now."
+else
   echo ">>> Install cancelled by user."
   exit 0
 fi
-
-cd /
-
-# remove default config files
-sudo rm etc/network/interfaces
-sudo rm etc/rc.local
-sudo rm etc/screenrc
-
-sudo git init
-sudo git remote add origin https://github.com/thor-mang/system_mpc_ubuntu.git
-sudo git fetch
-sudo git checkout -t origin/master
-
-echo "Installation completed! Please reboot system now."
