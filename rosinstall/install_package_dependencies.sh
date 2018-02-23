82cf87b85333723a2121135e2c2681c1dcf58be5
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2018-02-23 13:33:38 +0100

    updated moveit rosinstall

--- rosinstall/install_package_dependencies.sh
+++ rosinstall/install_package_dependencies.sh
@@ -24,26 +24,17 @@ ros-$ROS_DISTRO-octomap-msgs \
 ros-$ROS_DISTRO-octomap-ros \
 ros-$ROS_DISTRO-octomap-rviz-plugins \
 ros-$ROS_DISTRO-ompl \
-ros-$ROS_DISTRO-moveit \
-ros-$ROS_DISTRO-moveit-kinematics \
 ros-$ROS_DISTRO-ros-control \
 ros-$ROS_DISTRO-ros-controllers \
 ros-$ROS_DISTRO-camera-info-manager \
 ros-$ROS_DISTRO-urdfdom-py \
 ros-$ROS_DISTRO-urg-node \
-ros-$ROS_DISTRO-joy \
 ros-$ROS_DISTRO-laser-filters \
 ros-$ROS_DISTRO-pcl-ros \
 ros-$ROS_DISTRO-theora-image-transport \
 ros-$ROS_DISTRO-diagnostics \
-ros-$ROS_DISTRO-uvc-camera \
-ros-$ROS_DISTRO-openni2-camera \
-ros-$ROS_DISTRO-openni2-launch \
-ros-$ROS_DISTRO-geodesy \
 ros-$ROS_DISTRO-qt-build \
 ros-$ROS_DISTRO-humanoid-nav-msgs \
-ros-$ROS_DISTRO-polled-camera \
-ros-$ROS_DISTRO-trac-ik-kinematics-plugin \
 ros-$ROS_DISTRO-laser-assembler "
 
 apt_install $PACKAGES_TO_INSTALL
---
+++ rosinstall/optional/moveit_planning.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apt_install ros-$ROS_DISTRO-moveit ros-$ROS_DISTRO-moveit-kinematics ros-$ROS_DISTRO-moveit-visual-tools ros-$ROS_DISTRO-trac-ik-kinematics-plugin
