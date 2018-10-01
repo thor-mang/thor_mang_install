3562f50f0b36678f54743ef841d48d01779423dc
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2018-10-01 15:16:08 +0200

    updated perception install

--- rosinstall/install_scripts/install_ppc_system_script.sh
+++ rosinstall/install_scripts/install_ppc_system_script.sh
@@ -29,14 +29,14 @@ if [[ "$REPLY" = "y" || "$REPLY" = "Y" ]]; then
   bash -ic "thor update_make"
 
   # enable systemd init script
-  echo ">>> setup autostart"
+  echo ">>> Setup autostart"
   sudo systemctl enable thor.service
   # if desired to check if successfull -> "sudo systemctl is-enabled thor.service" should return "enabled"
 
   # enable shutdown and reboot without root password promt
   echo 'thor ALL=(ALL) NOPASSWD: /sbin/poweroff, /sbin/reboot, /sbin/shutdown' | sudo EDITOR='tee -a' visudo
 
-  echo ">>> install realsense"
+  echo ">>> Install realsense"
   sudo apt-get install ros-kinetic-realsense-camera
   
   echo "Installation completed! Please reboot system now."
