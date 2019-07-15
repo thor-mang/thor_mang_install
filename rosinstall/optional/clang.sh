e149df9481296d355404984a6a6fe91c530ebe5b
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2019-07-15 13:30:42 +0200

    updated clang install

--- rosinstall/optional/clang.sh
+++ rosinstall/optional/clang.sh
@@ -2,8 +2,17 @@
 
 source $ROSWSS_BASE_SCRIPTS/helper/helper.sh
 
-apt_install clang-format
+# replace standard install of clang with version 8
+apt_remove clang-format
+apt_install clang-format-8
 
+# create symlink
+cd /usr/bin
+if [ ! -f clang-format ]; then
+  sudo ln -s clang-format-8 clang-format
+fi
+
+# copy clang file
 cd $ROSWSS_ROOT/src
 if [ ! -f .clang-format ]; then
   ln -s ../.clang-format .clang-format
