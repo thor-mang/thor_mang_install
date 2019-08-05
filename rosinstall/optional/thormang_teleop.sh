829d64c4ece118afdfd8754410263dfe68441692
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2019-08-05 12:17:09 +0200

    added info text when installing thormang teleop

--- rosinstall/optional/thormang_teleop.sh
+++ rosinstall/optional/thormang_teleop.sh
@@ -2,5 +2,7 @@
 
 # add user to dialout in order to gain access to serial converter
 if ! getent group dialout | grep &>/dev/null "\b${USER}\b"; then
+    echo_info "Adding $USER to dialout group:"
     sudo usermod -aG dialout $USER
+    echo_info "Done"
 fi
