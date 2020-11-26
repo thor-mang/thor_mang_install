c613af7ea4aba9751ef3fd5ab752dd67875bf203
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2020-11-26 10:33:59 +0100

    Update clear_install.sh

--- rosinstall/install_scripts/clear_install.sh
+++ rosinstall/install_scripts/clear_install.sh
@@ -2,7 +2,7 @@
 
 cd $ROSWSS_ROOT
 echo ">>> Cleaning up old workspace files..."
-for f in .rosinstall* devel* build* install install_* .catkin_tools; do
+for f in .rosinstall* devel* build* install .catkin_tools; do
     [ -f $f ] && echo "rm -iv $f" && rm -i $f
     [ -d $f ] && echo "rm -Irv $f" && rm -Ir $f
 done
